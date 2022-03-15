51cb2b49988a3970dadf9dcd3cbefd3731d9aeab
joe lazarz <jlazarz87@gmail.com>
2022-03-15 10:14:51 -0400

    setting up for mobile msg

--- components/Messages/index.js
+++ components/Messages/index.js
@@ -3,9 +3,31 @@ import Head from "next/head"
 import MessageNav from "./MessageNav"
 import MessageView from "./MessageView"
 import ListView from "./ListView"
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+} from "react-device-detect"
 
 const Messages = () => {
   // mobile view returns separate set of components
+
+  if (isMobile) {
+    return (
+      <>
+        <Head>
+          <title>{"bbyDAO | messages"}</title>
+          <meta name="description" content="" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <div className="flex h-screen w-full flex-col">
+          <h1>Mobile Messages</h1>
+        </div>
+      </>
+    )
+  }
 
   return (
     <>
